aab2cd4d48519c9cd31a6939f972135575ea3863
Rostyslav <ros.kamach@gmail.com>
2019-03-20 16:51:25 +0200

    Modified, for listening vars fron Vagrantfile

--- progect_4/shell_script/ansible.sh
+++ progect_4/shell_script/ansible.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-BALANCE_IP=$1
-WEB2_IP=$2
-WEB1_IP=$3
-DB_IP=$4
-BALANCE_NAME=$5
-WEB2_NAME=$6
-WEB1_NAME=$7
-DB_NAME=$8
 # Install update all
 sudo yum install epel-release -y
 sudo yum update -y
@@ -14,30 +6,10 @@ sudo yum update -y
 sudo yum install ansible -y
 # Install dos2unix to fix script files
 sudo yum install dos2unix -y
-dos2unix /home/vagrant/scenario_1.sh
-dos2unix /home/vagrant/scenario_2.sh
-dos2unix /home/vagrant/playbook.yml
-dos2unix /home/vagrant/pb_all.yml
-dos2unix /home/vagrant/pb_nginx.yml
-dos2unix /home/vagrant/pb_haproxy.yml
-# Configuring Ansible
-sudo touch /home/vagrant/hosts.txt
-cat <<EOF | sudo tee -a /home/vagrant/hosts.txt
-[ALL]
-localhost ansible_connection=local
-$WEB1_NAME ansible_ssh_host=$WEB1_IP ansible_user=vagrant ansible_ssh_private_key_file=/home/vagrant/.ssh/$WEB1_IP.pem
-$WEB2_NAME ansible_ssh_host=$WEB2_IP ansible_user=vagrant ansible_ssh_private_key_file=/home/vagrant/.ssh/$WEB2_IP.pem
-$DB_NAME ansible_ssh_host=$DB_IP ansible_user=vagrant ansible_ssh_private_key_file=/home/vagrant/.ssh/$DB_IP.pem
-[WEB]
-$WEB1_NAME ansible_ssh_host=$WEB1_IP ansible_user=vagrant ansible_ssh_private_key_file=/home/vagrant/.ssh/$WEB1_IP.pem
-$WEB2_NAME ansible_ssh_host=$WEB2_IP ansible_user=vagrant ansible_ssh_private_key_file=/home/vagrant/.ssh/$WEB2_IP.pem
-[DB]
-$DB_NAME ansible_ssh_host=$DB_IP ansible_user=vagrant ansible_ssh_private_key_file=/home/vagrant/.ssh/$DB_IP.pem
-[LB]
-localhost ansible_connection=local
-EOF
+find /home/vagrant/provision/ -type f -print0 | xargs -0 sudo dos2unix --
+echo "Finished Database section"
 # Chainge Permitions for keys r-x------
-sudo chmod -R 500 /home/vagrant/.ssh/
+sudo chmod -R 500 /home/vagrant/provision/ssh_keys
 # Allow connecting without checking host
 sudo cat <<EOF | sudo tee -a /etc/ansible/ansible.cfg
 [defaults]
@@ -47,4 +19,5 @@ inventory = /home/vagrant/hosts.txt
 [ssh_connection]
 ssh_args = -o ControlMaster=auto -o ControlPersist=60s -o UserKnownHostsFile=/dev/null -o IdentitiesOnly=yes
 EOF
-sudo ansible-playbook playbook.yml
+#sudo chmod 777 /home/vagrant/vars.sh
+sudo ansible-playbook provision/ansible_playbooks/playbook.yml
